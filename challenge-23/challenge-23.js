b8ff59c80494b787c30d4fba8025cfc1d11f6752
Willians Marcono <wmarcono@globo.com>
2020-08-17 16:49:26 -0300

    'challenge-23'

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,3 +1,5 @@
+(function(win, doc){
+	'use strict';
 /*
 Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
 As regras são:
@@ -23,3 +25,100 @@ multiplicação (x), então no input deve aparecer "1+2x".
 input;
 - Ao pressionar o botão "CE", o input deve ficar zerado.
 */
+
+var $display = doc.querySelector('[data-js="display"]');
+
+var $buttonNumbers = doc.querySelectorAll('[data-js="buttonNumbers"]');
+		Array.prototype.forEach.call($buttonNumbers, function(button){
+			button.addEventListener('click', displayNumbers, false);
+	});
+
+var $buttonCE = doc.querySelector('[data-js="buttonCE"]');
+	$buttonCE.addEventListener('click', reset, false);
+		
+var $buttonOperations = doc.querySelectorAll('[data-js="buttonOperations"]');
+		Array.prototype.forEach.call($buttonOperations, function(button){
+			button.addEventListener('click', displayOperations, false);
+	});
+
+var $buttonEqual = doc.querySelector('[data-js="buttonEqual"]');
+	$buttonEqual.addEventListener('click', clickIgual, false);
+
+var valor1 = null;
+var valor2 = null;
+var operation = null;
+var opeIgual = null;
+
+function displayNumbers(){
+	if($display.value == '0' || $display.value == ''){
+		$display.value = '';
+		$display.value += validationDisplay(this.value);
+	}
+	else{
+		$display.value += this.value;
+	}
+}
+
+function displayOperations(){
+
+	if(!operation){
+	valor1 = $display.value;
+	operation = this.value;
+	$display.value = '';
+	}
+}
+
+function clickIgual(){
+	if(!opeIgual){
+		opeIgual = this.value;
+		aritimetica()
+		}
+	}
+
+function aritimetica(){
+	if(!valor2){
+		valor2 = $display.value;
+	}
+		$display.value = validateOperation(operation, valor1, valor2);
+		valor2 = 0
+		operation = null;
+		opeIgual = null;
+}
+
+function validationDisplay(valorBotao){
+	var validation;
+	if(valorBotao == '0'){
+		return validation = '0.';
+	}
+	return validation = valorBotao;
+}
+
+function validateOperation(valorBotao, valor1, valor2){
+	var validation;
+	switch(valorBotao){
+			case '+':
+				return Number(valor1) + Number(valor2);
+				break;
+
+			case '-':
+				return Number(valor1) - Number(valor2);
+				break;
+			
+			case 'x':
+				return Number(valor1) * Number(valor2);
+				break;
+			
+			case '/':
+				return Number(valor1) / Number(valor2);
+				break;
+			}
+}
+
+function reset(){
+	$display.value = '0';
+	valor2 = 0
+	operation = null;
+	opeIgual = null;
+}
+
+})(window, document);
