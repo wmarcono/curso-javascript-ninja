71193fb6e4b00513c8e8a458dc4c58796382d130
Willians Marcono <wmarcono@globo.com>
2020-08-18 15:12:23 -0300

    challenge-24

--- challenge-24/challenge-24.js
+++ challenge-24/challenge-24.js
@@ -1,3 +1,5 @@
+(function(win, doc){
+  'use strict';
 /*
 Nossa calculadora agora está funcional! A ideia desse desafio é modularizar
 o código, conforme vimos na aula anterior. Quebrar as responsabilidades
@@ -10,13 +12,19 @@ listeners de eventos, etc);
 mesma funcionalidade.
 */
 
-var $visor = document.querySelector('[data-js="visor"]');
-var $buttonsNumbers = document.querySelectorAll('[data-js="button-number"]');
-var $buttonsOperations = document.querySelectorAll('[data-js="button-operation"]');
-var $buttonCE = document.querySelector('[data-js="button-ce"]');
-var $buttonEqual = document.querySelector('[data-js="button-equal"]');
+var $visor = doc.querySelector('[data-js="visor"]');
+var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+var $buttonCE = doc.querySelector('[data-js="button-ce"]');
+var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
 
-Array.prototype.forEach.call($buttonsNumbers, function(button) {
+
+function initialize(){
+  initEvents();
+}
+
+function initEvents(){
+  Array.prototype.forEach.call($buttonsNumbers, function(button) {
   button.addEventListener('click', handleClickNumber, false);
 });
 Array.prototype.forEach.call($buttonsOperations, function(button) {
@@ -24,6 +32,8 @@ Array.prototype.forEach.call($buttonsOperations, function(button) {
 });
 $buttonCE.addEventListener('click', handleClickCE, false);
 $buttonEqual.addEventListener('click', handleClickEqual, false);
+
+}
 
 function handleClickNumber() {
   $visor.value += this.value;
@@ -37,39 +47,63 @@ function handleClickOperation() {
 function handleClickCE() {
   $visor.value = 0;
 }
-
+  
 function isLastItemAnOperation(number) {
-  var operations = ['+', '-', 'x', '÷'];
+  var operations = getOperations();
   var lastItem = number.split('').pop();
   return operations.some(function(operator) {
     return operator === lastItem;
   });
 }
 
-function removeLastItemIfItIsAnOperator(number) {
-  if(isLastItemAnOperation(number)) {
+function getOperations(){
+  return Array.prototype.map.call($buttonsOperations, function(button){
+    return button.value;
+  });
+}
+
+function removeLastItemIfItIsAnOperator(string) {
+  if(isLastItemAnOperation(string)) {
     return number.slice(0, -1);
   }
-  return number;
+  return string;
 }
 
 function handleClickEqual() {
   $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-  var allValues = $visor.value.match(/\d+[+x÷-]?/g);
-  $visor.value = allValues.reduce(function(accumulated, actual) {
+  var allValues = $visor.value.match(getRegexOperations());
+  $visor.value = allValues.reduce(calculateAllValues);
+}
+
+function getRegexOperations(){
+  return new RegExp('\\d+[' +getOperations().join('')+']?', 'g');
+}
+
+function calculateAllValues(accumulated, actual) {
     var firstValue = accumulated.slice(0, -1);
     var operator = accumulated.split('').pop();
     var lastValue = removeLastItemIfItIsAnOperator(actual);
-    var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+    var lastOperator = getLastOperator(actual);
+    return doOperation(operator, firstValue, lastValue) + lastOperator;   
+  }
+
+  function getLastOperator(value){
+    return isLastItemAnOperation(value) ? actual.split('').pop() : ''
+  }
+
+  function doOperation(operator, firstValue, lastValue){
     switch(operator) {
       case '+':
-        return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
+        return Number(firstValue) + Number(lastValue);
       case '-':
-        return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
+        return Number(firstValue) - Number(lastValue);
       case 'x':
-        return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
+        return Number(firstValue) * Number(lastValue);
       case '÷':
-        return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
+        return Number(firstValue) / Number(lastValue);
     }
-  });
-}
+  }
+
+initialize();
+
+})(window, document);
